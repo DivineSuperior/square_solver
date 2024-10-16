31ea7383b14d3aa4664dc5618b7946debc90cc8e
DivineSuperior <cooldanil40@gmail.com>
2024-10-16 22:25:55 +0300

    Update peniss.cpp

--- peniss.cpp
+++ peniss.cpp
@@ -9,15 +9,47 @@ int main()
     double discr = 0;
     double root_1 = 0;
     double root_2 = 0;
-
+    printf("enter the coefficient a: ");
     scanf("%lf", &a);
+    printf("enter the coefficient b: ");
     scanf("%lf", &b);
+    printf("enter the coefficient c: ");
     scanf("%lf", &c);
-
-    discr = pow(b,2) - 4*a*c;
-    root_1 = (-1*b + pow(discr,(0.5)))/2;
-    root_2 = (-1*b - pow(discr,(0.5)))/2;
-
-    printf("%lf,%lf", root_1,root_2);
+    if ((a == 0) || (b == 0) || (c == 0))
+    {
+    printf("The coefficients cannot be equal to 0.Try again dolbaeb! \n");
+    printf("enter the coefficient a: ");
+    scanf("%lf", &a);
+    printf("enter the coefficient b: ");
+    scanf("%lf", &b);
+    printf("enter the coefficient c: ");
+    scanf("%lf", &c);
+    }
+    /*
+    if ((c >= 'a') && (c <='z'))
+    {
+        printf("The coefficients cannot be letters. Try again debil! \n");
+        printf("enter the coefficient a: ");
+        scanf("%lf", &a);
+        printf("enter the coefficient b: ");
+        scanf("%lf", &b);
+        printf("enter the coefficient c: ");
+        scanf("%lf", &c);
+    }
+     */
+    discr = pow(b,2) - (4 * a * c);
+    if (discr < 0)
+        printf("the discriminant is less than 0 so there are no roots!\n");
+    if (discr == 0)
+        {
+        root_1 = -b / (2 * a);
+        printf("the only root = %lf \n", root_1);
+        }
+    if (discr > 0   )
+    {
+        root_1 = (-b + pow(discr,(0.5))) / (2 * a);
+        root_2 = (-b - pow(discr,(0.5))) / (2 * a);
+        printf("first root = %lf, second root = %lf\n", root_1,root_2);
+    }
     return 0;
 }
