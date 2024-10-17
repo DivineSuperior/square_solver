e899ccc39ce9275b847f06d00a830fa5fdaa6bcc
DivineSuperior <cooldanil40@gmail.com>
2024-10-17 17:50:11 +0300

    хуйнюшка недоделанная

--- peniss.cpp
+++ peniss.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <ctype.h>
 
 int main()
 {
@@ -9,22 +10,54 @@ int main()
     double discr = 0;
     double root_1 = 0;
     double root_2 = 0;
-    printf("enter the coefficient a: ");
+
+    printf("Enter the coefficient a: ");
     scanf("%lf", &a);
-    printf("enter the coefficient b: ");
+    printf("Enter the coefficient b: ");
     scanf("%lf", &b);
-    printf("enter the coefficient c: ");
+    printf("Enter the coefficient c: ");
     scanf("%lf", &c);
-    if ((a == 0) || (b == 0) || (c == 0))
+
+    double isALetter = isdigit (a);
+    double isBLetter = isdigit (b);
+    double isCLetter = isdigit (c);
+
+    while ( (isALetter == 0) || (isBLetter == 0) || (isCLetter == 0) )
     {
-    printf("The coefficients cannot be equal to 0.Try again dolbaeb! \n");
-    printf("enter the coefficient a: ");
-    scanf("%lf", &a);
-    printf("enter the coefficient b: ");
-    scanf("%lf", &b);
-    printf("enter the coefficient c: ");
-    scanf("%lf", &c);
+        printf("The coefficients cannot be a letter. Try again eblan!\n");
+        printf("Enter the coefficient a: ");
+        scanf("%lf", &a);
+        printf("Enter the coefficient b: ");
+        scanf("%lf", &b);
+        printf("Enter the coefficient c: ");
+        scanf("%lf", &c);
     }
+
+    while ((a == 0) || (b == 0) || (c == 0))
+    {
+        printf("The coefficients cannot be equal to 0. Try again dolbaeb! \n");
+        printf("Enter the coefficient a: ");
+        scanf("%lf", &a);
+        printf("Enter the coefficient b: ");
+        scanf("%lf", &b);
+        printf("Enter the coefficient c: ");
+        scanf("%lf", &c);
+    }
+
+
+    /*
+    while ( (isALetter == 1) || (isBLetter == 1) || (isCLetter == 1) )
+    {
+        printf("The coefficients cannot be a letter. Try again eblan!\n");
+        printf("Enter the coefficient a: ");
+        scanf("%lf", &a);
+        printf("Enter the coefficient b: ");
+        scanf("%lf", &b);
+        printf("Enter the coefficient c: ");
+        scanf("%lf", &c);
+    }
+    */
+
     /*
     if ((c >= 'a') && (c <='z'))
     {
@@ -37,19 +70,22 @@ int main()
         scanf("%lf", &c);
     }
      */
-    discr = pow(b,2) - (4 * a * c);
+    discr = (pow(b,2) - (4 * a * c));
+    double root_discr =pow(discr,(0.5));
     if (discr < 0)
-        printf("the discriminant is less than 0 so there are no roots!\n");
+        printf("The discriminant is less than 0 so there are no roots!\n");
+
     if (discr == 0)
-        {
-        root_1 = -b / (2 * a);
-        printf("the only root = %lf \n", root_1);
-        }
-    if (discr > 0   )
     {
-        root_1 = (-b + pow(discr,(0.5))) / (2 * a);
-        root_2 = (-b - pow(discr,(0.5))) / (2 * a);
-        printf("first root = %lf, second root = %lf\n", root_1,root_2);
+        root_1 = -b / (2 * a);
+        printf("The only root = %lf \n", root_1);
+
+    }
+    if (discr > 0)
+    {
+        root_1 = (-b + root_discr) / (2 * a);
+        root_2 = (-b - root_discr) / (2 * a);
+        printf("First root = %lf, Second root = %lf\n", root_1,root_2);
     }
     return 0;
 }
